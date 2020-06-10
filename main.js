4df482518a5fe8895ed33597f8c8775d4f4dce63
zanuarts <nuartea27@gmail.com>
2020-06-11 03:33:00 +0700

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "https://firebasestorage.googleapis.com/v0/b/vue-mastery.appspot.com/o/flamelink%2Fmedia%2F1578366170160_4.png?alt=media&token=e0829829-627b-44e1-9d96-fa22eaca1ac9",
+        altText: "A pair of socks",
+        inStock: false
+    }
+})
