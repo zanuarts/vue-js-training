20dd108374e58e347cdb7ef704156d8b8c84f365
zanuarts <nuartea27@gmail.com>
2020-06-11 15:12:46 +0700

    computed properties

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "https://firebasestorage.googleapis.com/v0/b/vue-mastery.appspot.com/o/flamelink%2Fmedia%2F1578366170160_4.png?alt=media&token=e0829829-627b-44e1-9d96-fa22eaca1ac9",
+        brand: 'Vue Mastery',
+        image: "./assets/vmSocks-green.png",
         altText: "A pair of socks",
         inStock: false,
         inventory: 100,
@@ -10,12 +11,34 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor:"green"
+                variantColor:"green",
+                variantImage:"./assets/vmSocks-green.png"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage:"./assets/vmSocks-blue.png"
             }
-        ]
+        ],
+        cart: 0,
+        selectedVariant: 0,
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateProduct(index){
+            this.selectedVariant = index
+            console.log(index)
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage,
+        },
+        
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        }
     }
 })
